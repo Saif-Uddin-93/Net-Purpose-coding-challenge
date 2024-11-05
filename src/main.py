0e6d0928ec41a4172a177df1f89560f3de6c62a0
Saif <admin@saifuddin.uk>
2024-11-05 19:40:06 +0000

    passes all flake8 checks

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
-def get_results(division:list[dict], n:int) -> str | None:
+def get_results(division: list[dict], n: int) -> str | None:
     # Your code here
 
     # If n is larger than half the size of the list, then return None.
     # This is to prevent teams appearing in both categories
-    if n*2 > len(division):
-        print(f"{n} is too big. Please enter a number that is a maximum of, {len(division)//2}")
+    max_n = len(division) // 2
+    if n * 2 > len(division):
+        print(f"{n}, is too big. Enter a number that is a maximum of, {max_n}")
         return
 
     # Sort teams in descending order according to their points.
-    sorted_teams = sorted(division, key=lambda team: team["points"], reverse=True)
+    sorted_teams = sorted(division, key=lambda t: t["points"], reverse=True)
     # promote list gets the first n teams
     promote = [team["name"] for team in sorted_teams[:n]]
     # relegate list gets the last n teams
     relegate = [team["name"] for team in sorted_teams[-n:]]
 
-    return "Promote:\n"+"\n".join(promote)+"\n\nRelegate:\n"+"\n".join(relegate)
+    return "Promote:\n" + "\n".join(promote) + "\n\nRelegate:\n" + \
+        "\n".join(relegate)
