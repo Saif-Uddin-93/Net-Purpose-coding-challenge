9a5b69827bb82c228511b1d2904a26ceef3f7904
Saif <admin@saifuddin.uk>
2024-11-06 09:57:43 +0000

    added condition to check that n is 1 or greater

--- src/main.py
+++ src/main.py
@@ -7,6 +7,9 @@ def get_results(division: list[dict], n: int) -> str | None:
     if n * 2 > len(division):
         print(f"{n}, is too big. Enter a number that is a maximum of, {max_n}")
         return
+    elif n<1:
+        print(f"{n}, is too small. Enter a number that is 1 or greater")
+        return
 
     # Sort teams in descending order according to their points.
     sorted_teams = sorted(division, key=lambda t: t["points"], reverse=True)
