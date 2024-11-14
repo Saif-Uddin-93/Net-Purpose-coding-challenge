2ec2a7ca7c6a3adbc8f155a9f254ce6a21500556
Full-Stack Dev <106098927+Saif-Uddin-93@users.noreply.github.com>
2024-11-14 14:49:31 +0000

    tidied code based on feedback from Net Purpose

--- src/main.py
+++ src/main.py
@@ -1,22 +1,36 @@
+from string import Template
+
+
 def get_results(division: list[dict], n: int) -> str | None:
     # Your code here
 
     # If n is larger than half the size of the list, then return None.
     # This is to prevent teams appearing in both categories
     max_n = len(division) // 2
-    if n * 2 > len(division):
+    if n > max_n:
         print(f"{n}, is too big. Enter a number that is a maximum of, {max_n}")
         return
-    elif n<1:
+    elif n < 1:
         print(f"{n}, is too small. Enter a number that is 1 or greater")
         return
 
     # Sort teams in descending order according to their points.
     sorted_teams = sorted(division, key=lambda t: t["points"], reverse=True)
     # promote list gets the first n teams
-    promote = [team["name"] for team in sorted_teams[:n]]
+    promote = "\n".join([team["name"] for team in sorted_teams[:n]])
     # relegate list gets the last n teams
-    relegate = [team["name"] for team in sorted_teams[-n:]]
+    relegate = "\n".join([team["name"] for team in sorted_teams[-n:]])
 
-    return "Promote:\n" + "\n".join(promote) + "\n\nRelegate:\n" + \
-        "\n".join(relegate)
+    results_msg = Template("""Promote:
+$promote
+
+Relegate:
+$relegate""").substitute({
+        "promote": promote,
+        "relegate": relegate
+    })
+
+    return results_msg
+
+    # return "Promote:\n" + "\n".join(promote) + "\n\nRelegate:\n" + \
+    #     "\n".join(relegate)
